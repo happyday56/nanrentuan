74d81ee9ede1435a68fa20986465245b7e93e167
lgh <106517651@qq.com>
2016-09-08 16:34:50 +0800

    init

---
+++ src/main/java/com/lgh/nanrentuan/entity/Article.java
@@ -0,0 +1,78 @@
+package com.lgh.nanrentuan.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * 最新文章
+ * Created by Administrator on 2016/5/2.
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 分类
+     */
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    private Category category;
+
+    /**
+     * 标题
+     */
+    @Column(length = 200)
+    private String title;
+
+    /**
+     * 图片地址
+     */
+    @Column(length = 200)
+    private String pictureUrl;
+    /**
+     * 内容
+     */
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private String content;
+
+    /**
+     * 摘要
+     */
+    @Column(length = 500)
+    private String summary;
+    /**
+     * 关键字
+     */
+    @Column(length = 200)
+    private String keywords;
+
+    /**
+     * 描述
+     */
+    @Column(length = 500)
+    private String description;
+
+
+    /**
+     * 上传时间
+     */
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date uploadTime;
+
+    /**
+     * 浏览次数
+     */
+    private Long views;
+}
---
+++ src/main/java/com/lgh/nanrentuan/entity/Category.java
@@ -0,0 +1,57 @@
+package com.lgh.nanrentuan.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Created by Administrator on 2016/5/2.
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "path")})
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /**
+     * 标题
+     */
+    @Column(length = 20)
+    private String name;
+    /**
+     * 分类路径，如孕早期（yzq）
+     * 根据标题首字母生产
+     */
+    @Column(length = 50)
+    private String path;
+
+    /**
+     * seo搜索标题
+     */
+    @Column(length = 200)
+    private String title;
+    /**
+     * seo关键字
+     */
+    @Column(length = 200)
+    private String keywords;
+    /**
+     * seo描述
+     */
+    @Column(length = 500)
+    private String description;
+
+    /**
+     * 排序号
+     */
+    private Integer sort;
+
+}
